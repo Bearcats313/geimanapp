02eb2870808e0a3f25d143a344ae8d64488f9d60
Brian <brennebn@gmail.com>
2025-07-16 12:35:27 -0400

    Update socket.js

--- geimanapp/public/hrms-dashboard/src/socket.js
+++ geimanapp/public/hrms-dashboard/src/socket.js
@@ -1,31 +1,15 @@
-import { io } from "socket.io-client"
-import { socketio_port } from "../../../../sites/common_site_config.json"
+import { io } from "socket.io-client";
+import { frappe } from "frappe-ui";
 
-import { getCachedListResource } from "frappe-ui/src/resources/listResource"
-import { getCachedResource } from "frappe-ui/src/resources/resources"
+// The problematic import is removed.
+// This simplified call tells socket.io to connect to the same server
+// that served the page, which is correct for a Frappe environment.
+const socket = io({
+	withCredentials: true,
+});
 
-export function initSocket() {
-	let host = window.location.hostname
-	let siteName = window.site_name
-	let port = window.location.port ? `:${socketio_port}` : ""
-	let protocol = port ? "http" : "https"
-	let url = `${protocol}://${host}${port}/${siteName}`
-	let socket = io(url, {
-		withCredentials: true,
-		reconnectionAttempts: 5,
-	})
+socket.on("connect", () => {
+	frappe.socket = socket;
+});
 
-	socket.on("hrms:refetch_resource", (data) => {
-		if (data.cache_key) {
-			let resource =
-				getCachedResource(data.cache_key) ||
-				getCachedListResource(data.cache_key)
-
-			if (resource) {
-				resource.reload()
-			}
-		}
-	})
-
-	return socket
-}
+export default socket;
