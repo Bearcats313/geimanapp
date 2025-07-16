edd8e9a4eb0bc7436f2f9e9499e0b90fcf8729e1
Brian <brennebn@gmail.com>
2025-07-16 13:54:02 -0400

    Update main.js

--- geimanapp/public/hrms-dashboard/src/main.js
+++ geimanapp/public/hrms-dashboard/src/main.js
@@ -1,146 +1,27 @@
-import { createApp } from "vue"
-import App from "./App.vue"
-import router from "./router"
-import { initSocket } from "./socket"
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+// MODIFICATION: Import the socket file directly to execute its code.
+import "./socket";
 
 import {
-	Button,
-	Input,
-	setConfig,
+	FrappeUI,
 	frappeRequest,
-	resourcesPlugin,
-	FormControl,
-} from "frappe-ui"
-import { translationsPlugin } from "./plugins/translationsPlugin.js"
-import EmptyState from "@/components/EmptyState.vue"
+	frappeEmitter,
+	setFrappeRouter,
+} from "frappe-ui";
+import "./assets/main.css";
 
-import { IonicVue } from "@ionic/vue"
+const app = createApp(App);
 
-import { session } from "@/data/session"
-import { userResource } from "@/data/user"
-import { employeeResource } from "@/data/employee"
+app.use(router);
+app.use(FrappeUI);
 
-import dayjs from "@/utils/dayjs"
-import getIonicConfig from "@/utils/ionicConfig"
+app.provide("$frappe", {
+	request: frappeRequest,
+	emitter: frappeEmitter,
+});
 
-import FrappePushNotification from "../public/frappe-push-notification"
+setFrappeRouter(router);
 
-/* Core CSS required for Ionic components to work properly */
-import "@ionic/vue/css/core.css"
-
-/* Theme variables */
-import "./theme/variables.css"
-
-import "./main.css"
-
-const app = createApp(App)
-const socket = initSocket()
-
-setConfig("resourceFetcher", frappeRequest)
-app.use(resourcesPlugin)
-app.use(translationsPlugin)
-
-app.component("Button", Button)
-app.component("Input", Input)
-app.component("FormControl", FormControl)
-app.component("EmptyState", EmptyState)
-
-app.use(router)
-app.use(IonicVue, getIonicConfig())
-
-if (session?.isLoggedIn && !employeeResource?.data) {
-	employeeResource.reload()
-}
-
-app.provide("$session", session)
-app.provide("$user", userResource)
-app.provide("$employee", employeeResource)
-app.provide("$socket", socket)
-app.provide("$dayjs", dayjs)
-
-const registerServiceWorker = async () => {
-	window.frappePushNotification = new FrappePushNotification("hrms")
-
-	if ("serviceWorker" in navigator) {
-		let serviceWorkerURL = "/assets/hrms/frontend/sw.js"
-		let config = ""
-
-		try {
-			config = await window.frappePushNotification.fetchWebConfig()
-			serviceWorkerURL = `${serviceWorkerURL}?config=${encodeURIComponent(
-				JSON.stringify(config)
-			)}`
-		} catch (err) {
-			console.error("Failed to fetch FCM config", err)
-		}
-
-		navigator.serviceWorker
-			.register(serviceWorkerURL, {
-				type: "classic",
-			})
-			.then((registration) => {
-				if (config) {
-					window.frappePushNotification.initialize(registration).then(() => {
-						console.log("Frappe Push Notification initialized")
-					})
-				}
-			})
-			.catch((err) => {
-				console.error("Failed to register service worker", err)
-			})
-	} else {
-		console.error("Service worker not enabled/supported by the browser")
-	}
-}
-
-router.isReady().then(async () => {
-	if (import.meta.env.DEV) {
-		await frappeRequest({
-			url: "/api/method/hrms.www.hrms.get_context_for_dev",
-		}).then(async (values) => {
-			if (!window.frappe) window.frappe = {}
-			window.frappe.boot = values
-		})
-	}
-
-	await translationsPlugin.isReady();
-	registerServiceWorker()
-	app.mount("#app")
-})
-
-router.beforeEach(async (to, _, next) => {
-	let isLoggedIn = session.isLoggedIn
-
-	try {
-		if (isLoggedIn) await userResource.reload()
-	} catch (error) {
-		isLoggedIn = false
-	}
-
-	if (!isLoggedIn) {
-		// password reset page is outside the PWA scope
-		if (to.path === "/update-password") {
-			return next(false)
-		} else if (to.name !== "Login") {
-			next({ name: "Login" })
-		}
-	}
-
-	if (isLoggedIn && to.name !== "InvalidEmployee") {
-		await employeeResource.promise
-		// user should be an employee to access the app
-		// since all views are employee specific
-		if (
-			!employeeResource?.data ||
-			employeeResource?.data?.user_id !== userResource.data.name
-		) {
-			next({ name: "InvalidEmployee" })
-		} else if (to.name === "Login") {
-			next({ name: "Home" })
-		} else {
-			next()
-		}
-	} else {
-		next()
-	}
-})
+app.mount("#app");
